4ef23bbec378710f6b52d4758e21379653b358ef
Zeryshka <zermaxim@gmail.com>
2021-02-28 22:24:58 +0300

    Добавлено логирование запросов

--- controller/productsController.php
+++ controller/productsController.php
@@ -4,6 +4,7 @@ function getAllProducts($db)
     $products = pg_query($db, "SELECT * FROM products");
     $products = pg_fetch_all($products);
     echo json_encode($products);
+    return "Success";
 }
 
 function getOneProduct($db, $id)
@@ -17,8 +18,18 @@ function getOneProduct($db, $id)
             "message" => "Товар не найден"
         ];
         echo json_encode($res);
+        return "Error";
     } else {
         $product = pg_fetch_all($product);
         echo json_encode($product);
+        return "Success";
     }
+}
+
+function createApiRequest($db, $result)
+{
+    $date = new DateTime();
+    $date = $date->format('Y-m-d H:i:s');
+    $ip = $_SERVER['REMOTE_ADDR'];
+    pg_insert($db, 'api_requests', ["ip" => $ip, "date" => $date, "status" => $result]);
 }
--- index.php
+++ index.php
@@ -1,6 +1,39 @@
 <?php
 include "config/db.php";
-include "routers/productsRouters.php";
+include "controller/productsController.php";
+
 header("Content-type: json/application");
 
-Products::getProducts($db, $_GET);
+$ip = $_SERVER['REMOTE_ADDR'];
+if ($ip == '127.0.0.1' || $ip == '178.210.79.186') {
+    $params = explode('/', $_GET['q']);
+    $type = $params[0];
+    $id = isset($params[1]) ? $params[1] : false;
+
+    switch ($type) {
+        case 'products':
+            if (!$id) {
+                $result = getAllProducts($db);
+                createApiRequest($db, $result);
+            } else {
+                $result = getOneProduct($db, $id);
+                createApiRequest($db, $result);
+            }
+            break;
+        default:
+            http_response_code(404);
+            $res = [
+                "status" => 404,
+                "message" => "Страница не найдена"
+            ];
+            echo json_encode($res);
+            break;
+    }
+} else {
+    http_response_code(403);
+    $res = [
+        "code" => 403,
+        "message" => "Недостаточно прав"
+    ];
+    echo json_encode($res);
+}
